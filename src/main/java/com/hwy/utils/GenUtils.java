11d793d92afd4a3dfe7016cb6f62d7845cbd4002
hweiyu <hwy_dod@163.com>
2018-08-07 16:20:12 +0800

    push resource code

--- src/main/java/com/hwy/utils/GenUtils.java
+++ src/main/java/com/hwy/utils/GenUtils.java
@@ -95,7 +95,7 @@ public class GenUtils {
         prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         Velocity.init(prop);
         String mainPath = config.getString("mainPath");
-        mainPath = StringUtils.isBlank(mainPath) ? "io.renren" : mainPath;
+        mainPath = StringUtils.isBlank(mainPath) ? "com.hwy" : mainPath;
         //封装模板数据
         Map<String, Object> map = new HashMap<>(16);
         map.put("tableName", tableEntity.getTableName());
